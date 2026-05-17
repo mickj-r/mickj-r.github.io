0ab85d83163c6a5ed4338ff83e745ef0871c10c4
Miqueias de jesus <mjrc1257@gmail.com>
2026-05-17 14:19:49 -0300

    refatora portifólio com tema dark minimalista e melhorias visuais

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const botao = document.getElementById("toggleTema")
 
+if (localStorage.getItem("tema") === "claro"){
+    document.body.classList.add("light-mode");
+}
 if(botao) {
     botao.addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
+
+        if (document.body.classList.contains("light-mode")) {
+            localStorage.setItem("tema", "claro");
+        } else {
+            localStorage.setItem("tema", "escuro");
+        }
+        
     });
 }
+
+const elementosReveal = document.querySelectorAll(".reveal");
+
+function mostrarElementos() {
+    elementosReveal.forEach((elemento) => {
+        const posicao = elemento.getBoundingClientRect().top;
+        const alturaTela = window.innerHeight * 0.85;
+
+        if (posicao < alturaTela) {
+            elemento.classList.add("ativo")
+        }
+    });
+}
+
+window.addEventListener("scroll", mostrarElementos);
+window.addEventListener("load", mostrarElementos);
